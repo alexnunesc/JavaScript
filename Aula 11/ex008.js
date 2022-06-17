240db82406823b2053ade7ac44c41eefaee350f5
alexnunesc <107014042+alexnunesc@users.noreply.github.com>
2022-06-17 12:52:22 -0300

    Aula 11

---
+++ Aula 11/ex008.js
@@ -0,0 +1,11 @@
+var vel = 67.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+//Condição simples
+if(vel > 65) {
+    console.log('Você está acima de 65km/h MULTADO')
+}
+
+if(vel < 65) {
+    console.log('Dirigar sempre usando cinto, Boa Viagem!')
+}
---
+++ Aula 11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'frança'
+console.log(`Vivendo em ${pais}`)
+//Condição composta
+if(pais == 'Brasil') {
+    console.log('Você e Brasileiro')
+}else{
+    console.log('Você é Estrageiro')
+}
