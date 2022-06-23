c1664921016c2218d9ceec39c828c800abc9e56c
alexnunesc <107014042+alexnunesc@users.noreply.github.com>
2022-06-23 08:13:05 -0300

    Aula 13

---
+++ Aula 13/ambienteTeste.js
@@ -0,0 +1,17 @@
+//Estrura WHILE. mesma coisa do Enquanto no VisualG
+/*
+let c = 1
+console.log('Comer Pizzar')
+while (c <= 8) {
+    console.log(`comer fatia n° ${c}`)
+    c++ //c = c + 1   mesma coisa do Enquanto no VisualG
+} 
+*/
+
+//Estrura DO WHILE. mesma coisa do Repita ate no VisualG
+let c = 1;
+console.log('Comer Pizziar');
+do{
+    console.log(`comer fatia n° ${c}`);
+    c++
+} while (c <= 5);
