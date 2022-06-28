46af277c98e7dc2edd002dee9d90255d110e0c20
alexnunesc <107014042+alexnunesc@users.noreply.github.com>
2022-06-27 21:46:48 -0300

    aula 16

---
+++ Aula 16/função01.js
@@ -0,0 +1,25 @@
+/*
+function parimpar(n){
+    if (n%2==0) {
+        return 'PAR'
+        
+    } else {
+        return 'IMPAR'
+    }
+}
+let res = parimpar(10)
+
+console.log(res)
+*/
+
+function parimpar(n){//função e parâmetro
+    if (n%2==0) {
+        return 'PAR'//returno
+        
+    } else {
+        return 'IMPAR'//returno
+    }
+}
+
+
+console.log(parimpar(10))//chama
---
+++ Aula 16/função02.js
@@ -0,0 +1,16 @@
+/*
+
+function soma(n1,n2){
+    return n1 + n2
+}
+
+console.log(soma(10 , 2))
+
+*/
+
+function soma(n1=0 , n2=0 ){
+    return n1 + n2
+}
+
+let s = soma(2)
+console.log(`${s}`)
