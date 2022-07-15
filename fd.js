7ab9befcb61313d64a325e379b67e6bb3eff1483
alexnunesc <107014042+alexnunesc@users.noreply.github.com>
2022-07-14 23:05:22 -0300

    Create fd.js

---
+++ fd.js
@@ -0,0 +1,25 @@
+/*
+function tamanho(Isabela,Luisa) {
+    if(Isabela > Luisa){
+        return 'Isabela calça mais'
+    }else if(Isabela < Luisa){
+        return 'Luisa calça mais'
+    }else{
+        return'Isabela e Luisa calçam o mesmo tamanho de sapato'
+    }
+}
+console.log(tamanho(35, 37))
+*/
+/*
+function tri(chanches){
+    let vetor = [1, 2, 3]
+    let i = 1
+    for(let pos = 0; pos < chanches.length; pos++){
+        i = chanches[pos]
+        vetor[pos] = 3*i
+    }
+    return vetor
+}
+console.log(tri([10, 20, 30]))
+
+*/
